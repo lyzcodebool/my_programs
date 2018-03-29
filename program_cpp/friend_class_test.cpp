3c07257bde21647490a4eb64e6ceccb9a88efc2a
lyzcodebool <lyzgithub@163.com>
2018-03-29 18:38:09 +0800

    c++ friend  class

---
+++ program_cpp/friend_class_test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+class Point1{
+public:
+    Point1(int a, int b):x(a), y(b){}
+    friend class Point2;
+private:
+    int x, y;
+};
+
+class Point2{
+public:
+    void print(const Point1 &a){
+        std::cout<<a.x<<" "<<a.y<<std::endl;
+    }
+private:
+    int x, y;
+};
+
+int main(void)
+{
+    Point2 a;
+    Point1 point(10, 10);
+    Point1 &b = point;
+
+    a.print(b);
+
+    return 0;
+}
